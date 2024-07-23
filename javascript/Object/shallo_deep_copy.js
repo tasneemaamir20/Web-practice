9f2374d434d1bfb845a2e10e7cc8355841efc00d
Tasneem Aamir <117052484+tasneemaamir20@users.noreply.github.com>
2024-07-23 14:53:17 +0530

    Add files via upload

--- javascript/Object/shallo_deep_copy.js
+++ javascript/Object/shallo_deep_copy.js
@@ -1,3 +1,4 @@
+console.log("=========== Shallow copy ========");
 let employee = {
   eid: "E102",
   ename: "Jack",
@@ -14,3 +15,18 @@ newEmpolyee.ename = "osama";
 
 console.log(newEmpolyee);
 console.log(employee);
+console.log("=========== Deep copy ========");
+let employee1 = {
+  eid: "E102",
+  ename: "Jack",
+  eaddress: "New York",
+  salary: 50000,
+};
+console.log("employee=>", employee1);
+let newEmployee1 = JSON.parse(JSON.stringify(employee1));
+console.log("newEmployee1=>", newEmployee1);
+
+console.log("====== afgter modification ============");
+newEmployee1.ename = "aamir";
+console.log(newEmployee1);
+console.log(employee1);
